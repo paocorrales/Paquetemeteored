4c428b2bd8d84c58b70ddbc0875e4b4c5ef3182f
PersonalAccessToken <inesmed004@gmail.com>
2024-10-18 22:05:48 -0300

    Actualizaciones y dependencias

--- ejercicios meteorologicos.R
+++ ejercicios meteorologicos.R
@@ -90,19 +90,3 @@ tabla_resumen_temperatura <- function(datos) {
   return(resumen)
 }
 
-
-grafico_temperatura_mensual <- function(datos, colores = NULL, titulo = "Temperatura") {
-  if (is.null(colores)) {
-    colores <- sample(colors(), length(unique(datos$mes)))
-  }
-
-  grafico <- datos %>%
-    group_by(estacion, mes) %>%
-    summarise(mean_temp = mean(temperatura_abrigo_150cm, na.rm = TRUE)) %>%
-    ggplot(aes(x = mes, y = mean_temp, color = estacion)) +
-    geom_line() +
-    scale_color_manual(values = colores) +
-    labs(title = titulo, x = "Mes", y = "Temperatura Promedio")
-
-  return(grafico)
-}
