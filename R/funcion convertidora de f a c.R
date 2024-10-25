f2fca4d97e953a5011cc3e2512bf747c36d94759
PersonalAccessToken <inesmed004@gmail.com>
2024-10-24 23:59:26 -0300

    Mejora de la funcion farenheit_a_centigrados

--- R/funcion convertidora de f a c.R
+++ R/funcion convertidora de f a c.R
@@ -2,24 +2,27 @@
 
 #' Convierte temperatura de farenheit a centigrados
 #'
-#' La función `convertir_farenheit_a_centigrados` toma una temperatura establecida en farenheit y la convierte a grados celcius.
+#' La función `farenheit_a_centigrados` toma una temperatura establecida en farenheit y la convierte a grados celcius.
 #'
-#' @param temp_max_hoy es el valor a convertir
+#' @param temperatura_fahrenheit es el valor a convertir
 #'
 #' @return
 #' Un vector numérico que es el valor de la temperatura en grados celcius
 #'
 #' @examples
-#' convertir_farenheit_a_centigrados(90)
-#' convertir_farenheit_a_centigrados(75)
+#' farenheit_a_centigrados(90)
+#' farenheit_a_centigrados(75)
 #'
 #' @export
 #'
-temp_max_hoy <- 53.7
-temp_max_hoy_centigrados <- (temp_max_hoy - 32) * 5/9
 
-
-convertir_farenheit_a_centigrados <- function(temp_max_hoy) {
-  (temp_max_hoy - 32) * 5/9
+farenheit_a_centigrados <- function(temperatura_fahrenheit) {
+  if (!is.numeric(temperatura_fahrenheit)){
+    cli::cli_abort(c(
+      "temperatura_fahrenheit debe ser numerico.",
+      "i" = "La variable ingresada es un {class(temperatura_fahrenheit)[1]."
+    ))
+  }
+  (temperatura_fahrenheit - 32) * 5/9
 }
-convertir_farenheit_a_centigrados(90)
+
